6652c5181f12288484663faf24fcc4bf8d3a9fb4
Guilherme Dias Tiede <guilhermediastiede@gmail.com>
2025-09-29 19:03:04 -0300

    fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,10 @@
 import { lazy } from "react";
 import IntroContent from "../../content/IntroContent.json";
-import MiddleBlockContent from "../../content/MiddleBlockContent.json";
 import AboutContent from "../../content/AboutContent.json";
-import MissionContent from "../../content/MissionContent.json";
-import ProductContent from "../../content/ProductContent.json";
 import ContactContent from "../../content/ContactContent.json";
 
 const Contact = lazy(() => import("../../components/ContactForm"));
-const MiddleBlock = lazy(() => import("../../components/MiddleBlock"));
+// const MiddleBlock = lazy(() => import("../../components/MiddleBlock")); // Mantido comentado para uso futuro
 const Container = lazy(() => import("../../common/Container"));
 const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
 const ContentBlock = lazy(() => import("../../components/ContentBlock"));
@@ -32,25 +29,7 @@ const Home = () => {
         icon="cat-graph.svg"
         id="about"
       />
-      {/* <ContentBlock
-        direction="right"
-        title={MissionContent.title}
-        content={MissionContent.text}
-        icon="product-launch.svg"
-        id="mission"
-      /> */}
-      {/* <ContentBlock
-        direction="left"
-        title={ProductContent.title}
-        content={ProductContent.text}
-        icon="waving.svg"
-        id="product"
-      /> */}
-      {/* <MiddleBlock
-        title={MiddleBlockContent.title}
-        content={MiddleBlockContent.text}
-        button={MiddleBlockContent.button}
-      /> */}
+      {/* Blocos adicionais (Mission, Product, MiddleBlock) mantidos comentados; reative quando necessário */}
       <Contact
         title={ContactContent.title}
         content={ContactContent.text}
